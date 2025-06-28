c825f8dff39dcdff4253e3eafb172667e7214adf
honey735 <beenhoneybeen@gmail.com>
2025-06-28 10:58:58 +0500

    Add files via upload

---
+++ HB.js
@@ -0,0 +1,5 @@
+ function showSurprise() {
+    const surprise = document.getElementById('surprise');
+    surprise.classList.remove('hidden');     // remove hidden class
+    surprise.style.display = 'block';        // make it appear
+  }
