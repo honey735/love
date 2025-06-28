beeac7e7e3de64c8df0e532d11c93e5780d99b57
honey735 <beenhoneybeen@gmail.com>
2025-06-28 11:04:13 +0500

    Delete HB.js

--- HB.js
+++
@@ -1,5 +0,0 @@
- function showSurprise() {
-    const surprise = document.getElementById('surprise');
-    surprise.classList.remove('hidden');     // remove hidden class
-    surprise.style.display = 'block';        // make it appear
-  }
